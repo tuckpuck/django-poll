a37a74de992a64ca48c6f929e0c4bba72a1e80c7
tuckpuck <tucker.triggs@yahoo.com>
2020-01-29 09:01:18 -0700

    set up template at /polls

--- pollapp/polls/views.py
+++ pollapp/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-# Create your views here.
+# Get questions and display them
+def index(request):
+    return render(request, 'polls/index.html')
+    
